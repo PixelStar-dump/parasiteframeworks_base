Evolution-X-AOSP/frameworks_base

Gesture nav: Add back menu action
Change-Id: Idaa6f7496b6babee5735c9eecf41bb8b95f4ace4
Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1396,6 +1396,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             case 17: // Go forward
                 EvolutionUtils.sendKeycode(KeyEvent.KEYCODE_FORWARD);
                 break;
+            case 18: // Menu
+                EvolutionUtils.sendKeycode(KeyEvent.KEYCODE_MENU);
+                break;
         }
     }
 

PixelStar-dump/parasiteframeworks_base

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1373,6 +1373,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             case 17: // Go forward
                 CustomUtils.sendKeycode(KeyEvent.KEYCODE_FORWARD);
                 break;
+            case 18: // Menu
+                CustomUtils.sendKeycode(KeyEvent.KEYCODE_MENU);
+                break;
         }
     }
 