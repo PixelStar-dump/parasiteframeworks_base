Evolution-X-AOSP/frameworks_base

Gesture nav: Add go forward action
* Used those for web browser

Change-Id: Id9a37d5f9f08cc682ac82d212fb09bda8ce7b1af
Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1393,6 +1393,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             case 16: // Recents
                 EvolutionUtils.sendKeycode(KeyEvent.KEYCODE_APP_SWITCH);
                 break;
+            case 17: // Go forward
+                EvolutionUtils.sendKeycode(KeyEvent.KEYCODE_FORWARD);
+                break;
         }
     }
 

PixelStar-dump/parasiteframeworks_base

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1370,6 +1370,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             case 16: // Recents
                 CustomUtils.sendKeycode(KeyEvent.KEYCODE_APP_SWITCH);
                 break;
+            case 17: // Go forward
+                CustomUtils.sendKeycode(KeyEvent.KEYCODE_FORWARD);
+                break;
         }
     }
 