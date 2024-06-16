36aac233c894d5cda2f664b88c33c61e11b00fdc
someone5678 <someone5678@users.noreply.github.com>
2024-06-16 15:55:40 +0900

    fixup! Long-press power while display is off for torch
    Change-Id: Ieef6c4b3ee7780ca2520a34acd99eb679e17488f

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1250,7 +1250,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 // See if we deferred screen wake because long press power for torch is enabled
                 if (mResolvedLongPressOnPowerBehavior == LONG_PRESS_POWER_TORCH &&
                         (!isScreenOn() || isDozeMode())) {
-                    wakeUpFromPowerKey(SystemClock.uptimeMillis());
+                    wakeUpFromWakeKey(SystemClock.uptimeMillis(), KEYCODE_POWER, /* isDown= */ false);
                 }
             }
         }
